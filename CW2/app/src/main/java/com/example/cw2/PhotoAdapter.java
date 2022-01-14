0ea9a956cc103f826598cda6264a41038ede40ad
1336735711 <77926863+1336735711@users.noreply.github.com>
2022-01-14 11:37:44 +0800

    4.0.12
    Upload and Retrieve Images Finish.

---
+++ CW2/app/src/main/java/com/example/cw2/PhotoAdapter.java
@@ -0,0 +1,61 @@
+package com.example.cw2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder> {
+    private Context mContext;
+    private List<Upload> mUploads;
+
+    public PhotoAdapter(Context context, List<Upload> uploads) {
+        mContext = context;
+        mUploads = uploads;
+    }
+
+    @NonNull
+    @Override
+    public PhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false);
+        return new PhotoViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotoViewHolder holder, int position) {
+        Upload uploadCurrent = mUploads.get(position);
+        holder.textViewName.setText(uploadCurrent.getName());
+        Picasso.get()
+                .load(uploadCurrent.getImageUrl())
+                .fit()
+                .centerInside()
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUploads.size();
+    }
+
+    public class PhotoViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewName;
+        public ImageView imageView;
+
+        public PhotoViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textPhotoName);
+            imageView = itemView.findViewById(R.id.imagePhotoUpload);
+        }
+    }
+}
