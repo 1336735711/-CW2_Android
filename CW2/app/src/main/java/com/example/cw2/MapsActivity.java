1e996ccb8799d298fec301230da73d241506fbdd
1336735711 <77926863+1336735711@users.noreply.github.com>
2022-01-14 20:29:21 +0800

    4.0.15
    can run，but only Require  user give Location Permission。hide Device Location。

--- CW2/app/src/main/java/com/example/cw2/MapsActivity.java
+++ CW2/app/src/main/java/com/example/cw2/MapsActivity.java
@@ -27,13 +27,45 @@ import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "initMap: initializing map");
+        mMap = googleMap;
+
+//        if (mLocationPermissionsGranted){
+//            getDeviceLocation();
+//
+//        }
+
+        // Add a marker in coordinate and move the camera
+        LatLng f_sss = new LatLng(22.501487962452135, 114.12799291314909);
+        LatLng s1_hksah = new LatLng(22.509554014683452, 114.10786739750866);
+        LatLng s2_syr = new LatLng(22.511119,114.111783);
+        LatLng s3_lv = new LatLng(22.509635979676624, 114.11307630895878);
+        LatLng s4_lwm = new LatLng(22.5157906495862, 114.11613589170472);
+        LatLng s5_dbcb = new LatLng(22.511663175793018, 114.1105069767066);
+        mMap.addMarker(new MarkerOptions().position(f_sss).title("First:Sheung Shui Station"));
+        mMap.addMarker(new MarkerOptions().position(s1_hksah).title("Stand1:Hau Ku Shek Ancestral Hall"));
+        mMap.addMarker(new MarkerOptions().position(s2_syr).title("Stand2:Sheung Yue River"));
+        mMap.addMarker(new MarkerOptions().position(s3_lv).title("Stand3:Long Valley"));
+        mMap.addMarker(new MarkerOptions().position(s4_lwm).title("Stand4:Large water mains (sewage treatment plants)"));
+        mMap.addMarker(new MarkerOptions().position(s5_dbcb).title("Stand5:豆美味豆花團(Delicious bean curd balls)"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(f_sss));
+
+    }
+
     private final static String TAG = "MapsActivity";
 
     private final static String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private final static String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private final static int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-    private Boolean mLocationPermissionsGranted = false;
+    private final static float DEFAULT_ZOOM = 14f;
 
+
+    private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
@@ -49,37 +81,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    private void getDeviceLocation(){
-        Log.d(TAG, "getDeviceLocation: getting the devices current location");
-
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        try {
-            if (mLocationPermissionsGranted){
-                Task location = mFusedLocationProviderClient.getLastLocation();
-                        location.addOnCompleteListener(new OnCompleteListener() {
-                            @Override
-                            public void onComplete(@NonNull Task task) {
-                                if (task.isSuccessful()){
-                                    Log.d(TAG, "onComplete: found location!");
-                                    Location currentLocation = (Location) task.getResult();
-
-                                }else{
-                                    Log.d(TAG, "onComplete: current location is null");
-                                    Toast.makeText(MapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-            
-        }catch (SecurityException e){
-            Log.e(TAG, "getDeviceLocation: SecurityException" +e.getMessage() );
-        }
-    }
-
-    private void moveCamera(LatLng latLng, float zoom){
-        Log.d(TAG, "moveCamera: moving the camera to: lat; " + latLng);
-    }
+//    private void getDeviceLocation(){
+//        Log.d(TAG, "getDeviceLocation: getting the devices current location");
+//
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+//
+//        try {
+//            if (mLocationPermissionsGranted){
+//                Task location = mFusedLocationProviderClient.getLastLocation();
+//                        location.addOnCompleteListener(new OnCompleteListener() {
+//                            @Override
+//                            public void onComplete(@NonNull Task task) {
+//                                if (task.isSuccessful()){
+//                                    Log.d(TAG, "onComplete: found location!");
+//                                    Location currentLocation = (Location) task.getResult();
+//
+//                                    moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+//
+//                                }else{
+//                                    Log.d(TAG, "onComplete: current location is null");
+//                                    Toast.makeText(MapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+//                                }
+//                            }
+//                        });
+//            }
+//
+//        }catch (SecurityException e){
+//            Log.e(TAG, "getDeviceLocation: SecurityException" +e.getMessage() );
+//        }
+//    }
+//
+//    private void moveCamera(LatLng latLng, float zoom){
+//        Log.d(TAG, "moveCamera: moving the camera to: lat; " + latLng);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+//    }
 
     private void initMap(){
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -144,26 +179,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "initMap: initializing map");
-        mMap = googleMap;
-
-        // Add a marker in coordinate and move the camera
-        LatLng f_sss = new LatLng(22.501487962452135, 114.12799291314909);
-        LatLng s1_hksah = new LatLng(22.509554014683452, 114.10786739750866);
-        LatLng s2_syr = new LatLng(22.511119,114.111783);
-        LatLng s3_lv = new LatLng(22.509635979676624, 114.11307630895878);
-        LatLng s4_lwm = new LatLng(22.5157906495862, 114.11613589170472);
-        LatLng s5_dbcb = new LatLng(22.511663175793018, 114.1105069767066);
-        mMap.addMarker(new MarkerOptions().position(f_sss).title("First:Sheung Shui Station"));
-        mMap.addMarker(new MarkerOptions().position(s1_hksah).title("Stand1:Hau Ku Shek Ancestral Hall"));
-        mMap.addMarker(new MarkerOptions().position(s2_syr).title("Stand2:Sheung Yue River"));
-        mMap.addMarker(new MarkerOptions().position(s3_lv).title("Stand3:Long Valley"));
-        mMap.addMarker(new MarkerOptions().position(s4_lwm).title("Stand4:Large water mains (sewage treatment plants)"));
-        mMap.addMarker(new MarkerOptions().position(s5_dbcb).title("Stand5:豆美味豆花團(Delicious bean curd balls)"));
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(f_sss));
-
-}}
+    }
